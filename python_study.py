76da0518b5eb6814e705ec6cf97c2ef423851008
Bolin Fu <fubolinzzz@gmail.com>
2024-07-24 18:21:08 +0800

    change2

--- python_study.py
+++ python_study.py
@@ -1,82 +1,89 @@
-#Python Syntax
-#1
+# Python Syntax
+# 1
 print("Hello, World!")
-#2
-if 5>2:
+# 2
+if 5 > 2:
     print("Five is greater than two!")
-#3
-x=5
-y="Hellow World!"
+# 3
+x = 5
+y = "Hellow World!"
 
-#Python Comments
+# Python Comments
 """
 This is a 
 multiline
 comment
 """
 
-#Python Variable
-#1
-x=5
-y="John"
+# Python Variable
+# 1
+x = 5
+y = "John"
 print(x)
 print(y)
-#2
-x=4
-x="sally"
+# 2
+x = 4
+x = "sally"
 print(x)
-#3
-x=str(3)
+# 3
+x = str(3)
 print(type(x))
-X=float(3)
+X = float(3)
 print(type(X))
 
-#Python Variables - Assign Multiple Values
-#1
-x,y,z,="Orange", "Banana", "Cherry"
+# Python Variables - Assign Multiple Values
+# 1
+x, y, z, = "Orange", "Banana", "Cherry"
 print(x)
 print(y)
 print(z)
-#2
-x=y=z="Orange"
-print(x,y,z)
-#3
-fruits=["apple", "banana", "cherry"]
-x,y,z= fruits
-print(x,y,z)
+# 2
+x = y = z = "Orange"
+print(x, y, z)
+# 3
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x, y, z)
 
-#Python - Output Variables
-#1
-x="Python is awesome"
+# Python - Output Variables
+# 1
+x = "Python is awesome"
 print(x)
-#2
+# 2
 x = "Python "
 y = "is "
 z = "awesome"
 print(x + y + z)
-#3
+# 3
 x = 5
 y = 10
 print(x + y)
-#4
+# 4
 x = 5
 y = "John"
 print(x, y)
 
-#Global Variables
-#1
-x="awesome"
+# Global Variables
+# 1
+x = "awesome"
+
 
 def myfunc():
-  x="fantastic"
-  print("Python is " + x)
+    x = "fantastic"
+    print("Python is " + x)
+
+
 myfunc()
 print("Python is " + x)
-#2
-x="awesome"
+# 2
+x = "awesome"
+
+
 def myfunc():
-  global x
-  x="fantastic"
+    global x
+    x = "fantastic"
+
+
 myfunc()
 print("Python is " + x)
 
