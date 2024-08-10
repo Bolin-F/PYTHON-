ef2fe513aebf5176e6ee681501eca91255065f80
Bolin Fu <fubolinzzz@gmail.com>
2024-08-11 02:53:48 +0800

    study

---
+++ boolean.py
@@ -0,0 +1,19 @@
+#boolean value
+print(10>9)
+print(10==9)
+print(10<9)
+
+a=200
+b=33
+if b>a:
+    print("b is bigger than a")
+else:
+    print("b is not bigger than a")
+
+print (bool("hellow"))
+print(bool(15))
+
+x="hello"
+y=15
+print(bool(x))
+print(bool(0))
